ee4312fb2c6de851e9688bfbb15f8b4f4a1805b4
Alumno <ulsa@iMacD115.local>
2018-11-30 01:56:49 -0700

    hay esta el api?

--- AppPortafolio/ListaProyectosController.swift
+++ AppPortafolio/ListaProyectosController.swift
@@ -12,6 +12,9 @@ import Alamofire
 
 class ListaProyectosController : UIViewController, UITableViewDataSource, UITableViewDelegate{
     
+    var urlProyecto = "http://portofolio-app.azurewebsites.net/?json=get_posts&post_type=proyecto"
+    var encontrada : String = ""
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return DatosProyectos.proyectos.count
     }
@@ -24,5 +27,76 @@ class ListaProyectosController : UIViewController, UITableViewDataSource, UITabl
         return celda
     }
     
+    override func viewDidLoad() {
+        Alamofire.request(urlProyecto).responseJSON { response in
+            if let dictRespuesta = response.result.value as? NSDictionary{
+                if let respuesta = dictRespuesta.value(forKey: "status") as? String {
+                    self.encontrada = respuesta
+                }
+                if self.encontrada == "ok" {
+                    if let post = dictRespuesta.value(forKey: "post") as? NSArray {
+                        for i in post {
+                            if let dictPost = i as? NSDictionary{
+                                if let fields = dictPost.value(forKey: "custom_fields") as? NSDictionary {
+                                    
+                                    var idProyecto : String = ""
+                                    var nombreProyecto : String = ""
+                                    var nombreUsuario : String = ""
+                                    var categoria : String = ""
+                                    var imagen : String = ""
+                                    var descripcion = ""
+                                    
+                                    if let id = fields.value(forKey: "id") as? NSArray {
+                                        for i in id{
+                                            if let proyecto = i as? String{
+                                                idProyecto = proyecto
+                                            }
+                                        }
+                                    }
+                                    if let nombre = fields.value(forKey: "name") as? NSArray {
+                                        for i in nombre{
+                                            if let proyecto = i as? String{
+                                                nombreProyecto = proyecto
+                                            }
+                                        }
+                                    }
+                                    if let usuario = fields.value(forKey: "user") as? NSArray {
+                                        for i in usuario{
+                                            if let proyecto = i as? String{
+                                                nombreUsuario = proyecto
+                                            }
+                                        }
+                                    }
+                                    if let categoriaProyecto = fields.value(forKey: "category") as? NSArray {
+                                        for i in categoriaProyecto{
+                                            if let proyecto = i as? String{
+                                                categoria = proyecto
+                                            }
+                                        }
+                                    }
+                                    if let imagenProyecto = fields.value(forKey: "image") as? NSArray {
+                                        for i in imagenProyecto{
+                                            if let proyecto = i as? String{
+                                                imagen = proyecto
+                                            }
+                                        }
+                                    }
+                                    if let descripcionProyecto = fields.value(forKey: "id") as? NSArray {
+                                        for i in descripcionProyecto{
+                                            if let proyecto = i as? String{
+                                                descripcion = proyecto
+                                            }
+                                        }
+                                    }
+                                    
+                                    DatosProyectos.proyectos.append(Proyecto(nombre: nombreProyecto, usuario: nombreUsuario, categoria: categoria, imagen: imagen, id: idProyecto, descripcion: descripcion, fechaCreacion: "", comentario: "", etiqueta: ""))
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
     
 }
